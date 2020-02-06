183db7b44367326fb537594e0beea9b0386be507
Mithun <mithun.y@sigmainfo.net>
2020-02-06 21:29:02 +0530

    Removing unwanted files and create statefull component

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    todos: [
+      {id:1, content: 'Buy some milk'},
+      {id:2, content: 'Play games now'}
+    ]
+  }
+  render(){
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
