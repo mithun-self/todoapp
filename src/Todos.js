cf9c8b47a3bed1e0f3bb0a3b7d304ce46fe7c9f1
Mithun <mithun.y@sigmainfo.net>
2020-02-06 22:05:50 +0530

    adding onclick delete

--- src/Todos.js
+++ src/Todos.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Todos = ({todos}) => {
+const Todos = ({todos, deleteTodo}) => {
     const todoList = todos.length ? (
         todos.map(todo => {
             return (
-                <div className="collection-item" key={todo.key}>
-                    <span>{todo.content}</span>
+                <div className="collection-item" key={todo.id}>
+                    <span onClick={()=> {deleteTodo(todo.id)} }>{todo.content}</span>
                 </div>
             )
         })
